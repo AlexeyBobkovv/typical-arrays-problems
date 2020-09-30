5108c4cab2be321637cce3d8ce050eb75cfe3f3b
AlexeyBobkov <alekseixxxbobkov@bk.ru>
2020-10-01 01:17:44 +0300

    init typiсal-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+const { sort } = require("semver");
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        //проверка на массив и есть ли что-то в нем
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let allParam = 0;
+
+        for (let i = 0; i < array.length; i++) {
+            allParam += array[i];
+        }
+        allParam = allParam / array.length;
+
+        return allParam;
+    }
+};
